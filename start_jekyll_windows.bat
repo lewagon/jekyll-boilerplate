4a6467108d60f7c119f56848e798052b2620a4db
Sebastien Saunier <seb@saunier.me>
2014-07-29 19:39:14 +0200

    Echo stuff on windows too

--- start_jekyll_windows.bat
+++ start_jekyll_windows.bat
@@ -1,1 +1,5 @@
+echo "\n\n\n\n-------------------------------------------------"
+echo "Open your browser and go to \033[1;32mhttp://localhost:4000\033[0;39m"
+echo "-------------------------------------------------\n\n\n\n"
+
 jekyll serve --watch
